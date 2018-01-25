d7bc2346369184881ec67e0f4705f5e7b52b58e8
Shady Ibrahim <s.ibrahim0114@gmail.com>
2018-01-24 21:11:02 -0500

    Pickup span tags of ingredient items to extract text

---
+++ main.py
@@ -0,0 +1,11 @@
+from bs4 import BeautifulSoup
+import requests
+
+r = requests.get("http://allrecipes.com/recipe/12682/apple-pie-by-grandma-ople/")
+
+raw_data = r.text
+
+soup = BeautifulSoup(raw_data, "html.parser")
+
+for item in soup.find_all('span', {"class": "recipe-ingred_txt added"}):
+    print(item)
