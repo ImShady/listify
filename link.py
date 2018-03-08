f6bc1e558779c3082dd266b68161d8c051f3c6ff
vhartvik <vhartviksen@gmail.com>
2018-03-07 21:07:50 -0500

    Created link class

--- link.py
+++ link.py
@@ -1,4 +1,31 @@
 from bs4 import BeautifulSoup
 import requests
+from ingredient import Ingredient
 
 class Link():
+	def __init__(self, url):
+		self.url = url
+		self.parse_ings(self.fetch_ings(url))
+
+	def parse_ings(self, ing_strings):
+		self.parsed_ings = []
+		units = ["cup", "cups", "ounce", "oz", "gram", "tablespoon", "tablespoons", "tbsp", "teaspoon", "teaspoons","tsp"]
+		
+		for ing in self.ing_strings:
+			split_ing = ing.split()
+			ing = Ingredient(split_ing[0], split_ing[1] if split_ing[1] in units else "item", ' '.join(split_ing[2:]))
+			self.parsed_ings.append(ing)
+
+		return self.parsed_ings
+
+	def fetch_ings(self, url):
+		r = requests.get(url) # this is how you comment in Python! =)
+
+		raw_data = r.text
+		self.ing_strings = []
+		soup = BeautifulSoup(raw_data, "html.parser")
+
+		for item in soup.find_all('span', {"class": "recipe-ingred_txt added"}):
+		    self.ing_strings.append(item.text)
+
+		return self.ing_strings
--- main.py
+++ main.py
@@ -1,39 +1,13 @@
 from bs4 import BeautifulSoup
 import requests
 from ingredient import Ingredient
-
-def fetch_ings(url):
-	r = requests.get(url) # this is how you comment in Python! =)
-
-	raw_data = r.text
-	ing_strings = []
-	soup = BeautifulSoup(raw_data, "html.parser")
-
-	for item in soup.find_all('span', {"class": "recipe-ingred_txt added"}):
-	    ing_strings.append(item.text)
-
-	return ing_strings
-
-def convert_to_grams():
-
-
-def parse_ings(ing_strings):
-	parsed_ings = []
-	units = ["cup", "cups", "ounce", "oz", "gram", "tablespoon", "tablespoons", "tbsp", "teaspoon", "teaspoons","tsp"]
-	
-	for ing in ing_strings:
-		split_ing = ing.split()
-		parsed_ing = {}
-		parsed_ing['amount'] = split_ing[0] 
-		parsed_ing['unit'] = split_ing[1] if split_ing[1] in units else "item"
-		parsed_ing['name'] = ' '.join(split_ing[2:])
-		parsed_ings.append(parsed_ing)
-
-	return parsed_ings
+from link import Link
 
 def main():
-	test_url = "http://allrecipes.com/recipe/12682/apple-pie-by-grandma-ople/"
-	print(parse_ings(fetch_ings(test_url)))
+	url = Link("http://allrecipes.com/recipe/12682/apple-pie-by-grandma-ople/")
+	for ing in url.parsed_ings:
+		print(str(ing.amount) + " " + ing.unit + " " + ing.name)
+
   
 if __name__== "__main__":
   main()
