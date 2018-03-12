f5133050d39848b612d79251dbc76f301d1b4ab6
vhartvik <vhartviksen@gmail.com>
2018-03-12 18:48:13 -0400

    read in url as argument

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from bs4 import BeautifulSoup
 import requests
+import sys
 from ingredient import Ingredient
 from link import Link
 
 def main():
-	url = Link("http://allrecipes.com/recipe/12682/apple-pie-by-grandma-ople/")
+	url = Link(sys.argv[1])
+	# url = Link("http://allrecipes.com/recipe/12682/apple-pie-by-grandma-ople/")
 	for ing in url.parsed_ings:
 		print(str(ing.amount) + " " + ing.unit + " " + ing.name)
 
