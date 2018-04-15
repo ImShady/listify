81a66c76434f383ca489af6a796fddf958694eac
vhartvik <vhartviksen@gmail.com>
2018-04-15 15:49:58 -0400

    added convert to grams and convert from grams capabilities for metric measurements.  May want to get more precise conversions

--- ingredient.py
+++ ingredient.py
@@ -1,3 +1,15 @@
+# grams per measurement (metric conversions)
+per_gr_coversion = {
+	'tsp': 5, 
+	'tbsp': 15, 
+	'cup': 250, 
+	'pint': 473.176475, 
+	'quart': 946.35295, 
+	'kg': 1000, 
+	'oz': 28.349523125, 
+	'lb': 453.59237, 
+	'mg': 0.001
+}
 
 class Ingredient():
 
@@ -7,11 +19,14 @@ class Ingredient():
 		self.name = name
 
 	def convert_to_decimal(self, amount):
-	    try:
-	        return float(amount)
-	    except ValueError:
-	        num, denom = amount.split('/')
-	        return float(num) / float(denom)
+		try:
+			return float(amount)
+		except ValueError:
+			num, denom = amount.split('/')
+			return float(num) / float(denom)
 
-	def convert_to_grams(self):
-		pass
+	def convert_to_grams(self, amount, unit):
+		return float(amount) * dict.get(per_gr_coversion, unit, 1)
+
+	def convert_from_grams(self, amount, unit):
+		return float(amount) / dict.get(per_gr_coversion, unit, 1)
--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from ingredient import Ingredient
 from link import Link
 
 def main():
-	url = Link(sys.argv[1])
+	url = Link(sys.argv[1]) # send in URL as a parameter; the line below is useful for testing
 	# url = Link("http://allrecipes.com/recipe/12682/apple-pie-by-grandma-ople/")
 	for ing in url.parsed_ings:
 		print(str(ing.amount) + " " + ing.unit + " " + ing.name)
